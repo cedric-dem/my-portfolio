9d5760137733ac7a2b52ae30341bc9335f3cea37
cedric <demuelenaere.cedric@gmail.com>
2025-06-03 15:13:11 +0200

    create HomePage content

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,8 +4,10 @@
 
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>Home page</p>
+    <h1>Who Am I ?</h1>
+    <p>My name is Cédric, I was born on the 20th of August 1999. I live in Belgium</p>
+    <h1>Hello World !</h1>
+    <p>I have always been fascinated by computer, since my childhood I have always been experimenting with softwares</p>
   </div>
 </template>
 
