273cb05d809a7d577cf8112d7afe8236f9399208
cedric <demuelenaere.cedric@gmail.com>
2025-08-28 12:03:46 +0200

    add LinkedIn link + add profile picture + update logo

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -8,7 +8,10 @@
     <p>If you would like to discuss with me, feel free to join me via  </p>
 
     <h2>E-mail</h2>
+
     <p><a :href="'mailto:' + 'demuelenaere.cedric@gmail.com'" class="email-link">{{ "demuelenaere.cedric@gmail.com" }}</a></p>
+
+    <p><a :href="'https://be.linkedin.com/in/cedric-de-muelenaere'" target="_blank" rel="noopener noreferrer" class="linkedin-link"> My LinkedIn </a> </p>
 
     <h2>Phone</h2>
     <p>(+32) 471/50.10.37</p>
